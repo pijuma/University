a7a765f75cba0ed5f6d154ca0698301b7ce528b7
Pietra <73311085+pijuma@users.noreply.github.com>
2023-06-19 16:00:40 -0300

    Create matriz_vetor.c

---
+++ ICC1/Alocacao_dinamica/matriz_vetor.c
@@ -0,0 +1,22 @@
+// alocar matriz como vetor 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int n, m ; scanf("%d%d", &n, &m) ;
+
+    int *v ; 
+
+    v = (int *) malloc(n*m*sizeof(int)) ;
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++) scanf("%d", &v[(i*n) + j]) ;
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++) printf("%d ", v[(i*n) + j]) ;
+        printf("\n") ;
+    }
+
+}
