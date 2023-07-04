a32f4aa98372dba80ad2bb1c25bff7c8ab21fa26
Pietra <73311085+pijuma@users.noreply.github.com>
2023-07-04 20:30:00 -0300

    Update matriz_vetor.c

--- ICC1/Alocacao_dinamica/matriz_vetor.c
+++ ICC1/Alocacao_dinamica/matriz_vetor.c
@@ -11,11 +11,11 @@ int main(){
     v = (int *) malloc(n*m*sizeof(int)) ;
 
     for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < m ; j++) scanf("%d", &v[(i*n) + j]) ;
+        for(int j = 0 ; j < m ; j++) scanf("%d", &v[(i*m) + j]) ;
     }
 
     for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < m ; j++) printf("%d ", v[(i*n) + j]) ;
+        for(int j = 0 ; j < m ; j++) printf("%d ", v[(i*m) + j]) ;
         printf("\n") ;
     }
 
