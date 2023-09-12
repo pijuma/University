bdc566f95b64405347cefeca5e63b31cb613d656
Pietra <73311085+pijuma@users.noreply.github.com>
2023-09-12 13:35:54 -0300

    Create item.c

---
+++ ED/item.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "item.h"
+
+struct item_{ 
+    int chave ; 
+};
+
+ITEM *item_criar (int chave){
+    
+    ITEM *a = NULL ; 
+    a = (ITEM * ) malloc(sizeof(ITEM)) ;
+    
+    if(a != NULL){
+        a->chave = chave ; 
+        return a ; 
+    }
+    
+    return NULL ; 
+
+}
+
+bool item_apagar(ITEM **item){
+    
+    if(*item != NULL){ 
+        free(*item) ;
+        *item = NULL ;
+        return true ; 
+    }
+    
+    return false ; 
+
+}
+
+int item_get_chave(ITEM *item){
+    if(item != NULL){
+        return item -> chave ; 
+    }
+    exit(1) ; // cuidado para nao dar BO  
+}
+
+
+bool item_set_chave(ITEM *item, int chave){
+
+    if(item != NULL){
+        item -> chave = chave ; 
+        return true ; 
+    }
+
+    return false ; 
+
+}
+
+void item_imprimir(ITEM *item){
+    if(item != NULL) printf("Valor %d\n", item -> chave) ;
+}
