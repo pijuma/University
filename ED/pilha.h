f3582788e7ad3524aa2aa95c5c4bf06c1f168a43
Pietra <73311085+pijuma@users.noreply.github.com>
2023-09-13 22:32:00 -0300

    Create pilha.h

---
+++ ED/pilha.h
@@ -0,0 +1,26 @@
+#ifndef PILHA_H
+	#define PILHA_H 
+    
+    #include <stdbool.h>
+    #include "item.h"
+    #define maxn (int)(20005) 
+
+    typedef struct pilha_ PILHA ; 
+
+    PILHA *pilha_criar() ; 
+
+    bool pilha_apagar(PILHA *pilha) ; 
+
+    ITEM *top(PILHA *pilha) ; 
+
+    ITEM *pop(PILHA *pilha) ; 
+
+    bool push(PILHA *pilha, ITEM *x) ; 
+
+    int pilha_tamanho(PILHA *pilha) ; 
+
+    bool pilha_vazia(PILHA *pilha) ;
+    bool pilha_cheia(PILHA *pilha) ; 
+
+
+#endif 
