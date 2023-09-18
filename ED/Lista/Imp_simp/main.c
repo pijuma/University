ea46234e46d233588953251f2bbefe0f5c6b9a88
Pietra <73311085+pijuma@users.noreply.github.com>
2023-09-18 09:15:09 -0300

    Update main.c

--- ED/Lista/Imp_simp/main.c
+++ ED/Lista/Imp_simp/main.c
@@ -8,11 +8,15 @@ int main(){
 
     for(int i = 1  ; i <= n ; i++){
         int b ; scanf("%d", &b) ; 
-        ITEM *at = item_criar(b) ;
-        if(!lista_inserir(lista, at)) return 0 ; 
+        ITEM *at = item_criar(b) ; 
+        if(!lista_inserir(lista, busca(lista, b) , at)) return 0 ; 
     }
 
-    lista_imprime(lista) ; 
+    for(int i = 1 ; i <= n ; i++){
+        int b ; scanf("%d", &b) ;
+        if(!lista_remove(lista, busca(lista, b))) return 0 ; 
+        lista_imprime(lista) ;
+    }
 
     int p ; scanf("%d", &p) ;
 
