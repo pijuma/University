6d9f8a279863c709ad6dc6240fe85d75c594dc98
Pietra <73311085+pijuma@users.noreply.github.com>
2023-09-18 09:14:32 -0300

    Update lista.c

--- ED/Lista/Imp_simp/lista.c
+++ ED/Lista/Imp_simp/lista.c
@@ -15,10 +15,15 @@ LISTA *criar(){
 
 }
 
-bool lista_inserir(LISTA *lista, ITEM *a){ // insere no fim
-    
-    if(lista != NULL && !lista_cheia(lista)){
-        lista->list[lista->fim] = a ; 
+bool lista_inserir(LISTA *lista, int pos, ITEM *a){ // insere em pos - O(N)
+
+    if(pos == -1) pos = lista->fim ; 
+
+    if(lista != NULL && !lista_cheia(lista) && pos <= lista -> fim){
+        for(int i = lista-> fim - 1 ; i >= pos ; i--){
+            lista -> list[i+1] = lista -> list[i] ;
+        }
+        lista->list[pos] = a ; 
         lista->fim++ ; 
         return 1 ; 
     }
@@ -29,15 +34,34 @@ bool lista_inserir(LISTA *lista, ITEM *a){ // insere no fim
 
 ITEM *lista_remove(LISTA *lista, int pos){ // tira do começo 
     
-    if(lista != NULL && !lista_vazia(lista)){
-        ITEM *b = lista->list[lista->ini] ;
-        lista->list[lista->ini] = NULL ; 
-        for(int i = lista->ini + 1 ; i < lista->fim; i++) lista->list[i-1] = lista->list[i] ;
+    if(lista != NULL && !lista_vazia(lista) && pos >= lista->ini && pos <= lista->fim-1){
+        ITEM *b = lista->list[pos] ;
+        lista->list[pos] = NULL ; 
+        for(int i = pos + 1 ; i < lista->fim; i++) lista->list[i-1] = lista->list[i] ;
         lista->fim-- ; 
         return b ;
     }
 
     return NULL ; 
+
+}
+
+int busca(LISTA *lista, int a){ // achar o 1o elemento q tem valor >=a - onde irei colocar p manter a ordem 
+
+    // busca binaria - O(log(n))
+
+    int ini = lista -> ini, fim = lista->fim - 1, mid, best = -1 ; 
+
+    while(ini <= fim){
+
+        mid = (ini + fim)>>1 ;
+
+        if(item_get_chave(lista->list[mid]) >= a) best = mid, fim = mid - 1 ; 
+        else ini = mid + 1 ; 
+
+    }
+
+    return best ; 
 
 }
 
