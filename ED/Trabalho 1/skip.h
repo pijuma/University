1ed8fc62412cacce775a3cfce6a179f19e072d56
Pietra <73311085+pijuma@users.noreply.github.com>
2023-10-06 18:07:38 -0300

    Update skip.h

--- ED/Trabalho 1/skip.h
+++ ED/Trabalho 1/skip.h
@@ -1,1 +1,14 @@
+#ifndef LISTA_H
+    #define LISTA_H
+    
+    #include "item.h"
 
+    typedef struct SKIP_L SKIP ; 
+          
+    SKIP *skip_criar();
+    ITEM *skip_busca_key(SKIP *skip, char *pal);
+    bool skip_inserir(SKIP *skip, ITEM *a);
+    void imprime_skip(SKIP *skip);
+    bool skip_alterar(SKIP *skip, ITEM *a);
+    
+#endif
