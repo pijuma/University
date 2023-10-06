a39498722928aa740295e9627c61f9cf75dc30bf
Pietra <73311085+pijuma@users.noreply.github.com>
2023-10-06 18:06:58 -0300

    Update main.c

--- ED/Trabalho 1/main.c
+++ ED/Trabalho 1/main.c
@@ -11,17 +11,34 @@ int main(){
 
     for(int i = 0 ; i < n ; i++){
         char a[55], b[145] ;
-        scanf(" %s", a) ;
+        scanf(" %s", a) ; 
         scanf(" %[^\n]s", b) ;
-        printf("%s %s", a, b) ;
+        printf("%s %s\n", a, b) ; 
         ITEM *at = item_criar(a, b) ;
         skip_inserir(lista, at) ;
     }
-    /*
+    
+    //imprime_skip(lista) ; 
+    
     char c[55] ;
-    scanf("%s", c) ; 
+    scanf(" %s", c) ; 
     ITEM *r = skip_busca_key(lista, c) ;
-    char *resp = item_get_verb(r) ;
-    printf("%s\n", resp) ; */
+    if(r == NULL) printf("Nao achei\n") ; 
+    else{
+        char *resp = item_get_verb(r) ;
+        printf("%s\n", resp) ; 
+    }
+
+    char d[145] ;
+    scanf(" %s", d) ;
+    ITEM *neww = item_criar(c, d) ;
+    skip_alterar(lista, neww) ;  
+    
+    r = skip_busca_key(lista, c) ;
+    if(r == NULL) printf("Nao achei\n") ; 
+    else{
+        char *resp = item_get_verb(r) ;
+        printf("%s\n", resp) ; 
+    }
 
 }
