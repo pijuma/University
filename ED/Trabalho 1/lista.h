51beae61613812c66a7c5ae751b87a877dbd7629
Pietra <73311085+pijuma@users.noreply.github.com>
2023-10-07 12:09:45 -0300

    Update lista.h

--- ED/Trabalho 1/lista.h
+++ ED/Trabalho 1/lista.h
@@ -1,12 +1,18 @@
-#ifndef LISTA_H
-    #define LISTA_H
+#ifndef ITEM_H 
+    #define ITEM_H 
+    #include <stdbool.h>
+
+    typedef struct item_ ITEM ;
+
+    ITEM *item_criar (char pal[], char sig[]) ;
+
+    bool item_apagar(ITEM **item) ;
+
+    char *item_get_key(ITEM *item) ;
+    char *item_get_verb(ITEM *item) ;
+    void item_imprimir_key(ITEM *a);
+    void item_imprimir_verb(ITEM *a);
+    bool item_set_verb(ITEM *item, char *sig) ;
+        
     
-    #include "item.h"
-
-    typedef struct SKIP_L SKIP ; 
-          
-    SKIP *skip_criar();
-    ITEM *skip_busca_key(SKIP *skip, char *pal);
-    bool skip_inserir(SKIP *skip, ITEM *a);
-
 #endif
