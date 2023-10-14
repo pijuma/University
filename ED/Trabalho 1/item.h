f488bd4156c5beaec4b1cfad07fa29bfabda069d
Pietra <73311085+pijuma@users.noreply.github.com>
2023-10-14 19:20:41 -0300

    Update item.h

--- ED/Trabalho 1/item.h
+++ ED/Trabalho 1/item.h
@@ -1,18 +1,17 @@
 #ifndef ITEM_H 
     #define ITEM_H 
-    #include <stdbool.h>
-
+    // TAD item padrão 
     typedef struct item_ ITEM ;
 
     ITEM *item_criar (char pal[], char sig[]) ;
 
-    bool item_apagar(ITEM **item) ;
+    int item_apagar(ITEM **item) ;
 
     char *item_get_key(ITEM *item) ;
     char *item_get_verb(ITEM *item) ;
     void item_imprimir_key(ITEM *a);
     void item_imprimir_verb(ITEM *a);
-    bool item_set_verb(ITEM *item, char *sig) ;
+    int item_set_verb(ITEM *item, char *sig) ;
         
     
 #endif
