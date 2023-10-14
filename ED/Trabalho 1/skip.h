6a5a5412fc29ad03c8445128e5216fdb318e441e
Pietra <73311085+pijuma@users.noreply.github.com>
2023-10-14 19:20:18 -0300

    Update skip.h

--- ED/Trabalho 1/skip.h
+++ ED/Trabalho 1/skip.h
@@ -1,17 +1,17 @@
 #ifndef LISTA_H
     #define LISTA_H
-    
+    // TAD lista padrão 
     #include "item.h"
 
     typedef struct SKIP_L SKIP ; 
           
     SKIP *skip_criar();
     ITEM *skip_busca_key(SKIP *skip, char *pal);
-    bool skip_inserir(SKIP *skip, ITEM *a);
+    int skip_inserir(SKIP *skip, ITEM *a);
     void imprime_skip(SKIP *skip);
-    bool skip_alterar(SKIP *skip, ITEM *a);
-    bool skip_remover(SKIP *skip, char *a);
-    bool skip_imprime_char(SKIP *skip, char a);
+    int skip_alterar(SKIP *skip, ITEM *a);
+    int skip_remover(SKIP *skip, char *a);
+    int skip_imprime_char(SKIP *skip, char a);
     void skip_apagar(SKIP **skip);
     
 #endif
