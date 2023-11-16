0c9cd3875a2b546a9f0b10174401db51a7eb175c
Pietra <73311085+pijuma@users.noreply.github.com>
2023-11-16 16:40:04 -0300

    Update shell_sort.c

--- icc2/shell_sort.c
+++ icc2/shell_sort.c
@@ -1,26 +1,29 @@
 #include<stdio.h>
-#include<stdlib.h> 
 #include<stdbool.h>
 
 void shell(int *v, int n){
 
     int h = 1 ; 
 
-    for(h = 1 ; h < n/3 ; h = (h*3) + 1) ; 
+    // insertion eh o shell qd h = 1 sempre 
 
-    while(h > 0){ 
-        for(int i = 1 ; i < n ; i++){
-            int valor_at = v[i] ; 
-            int j = i ; // considera q ate j ta ordenado 
-            while(v[j-h] > valor_at){ // se eu achar algum kra antes de mim q eh maior q o meu ele tem q vir dps 
-                v[j] = v[j-h] ; // passo ele p frente 
-                j-=h ; 
-                if(j < h) break ;
-            }
-            v[j] = valor_at ; 
-        }
+    for(; h < n ; h = (3*h) + 1) ; // calcular h inicial 
+
+    while(h > 0){
+
         h = (h-1)/3 ;
+
+        for(int i = 0 ; i < n ; i++){
+            int aux = v[i], j = i ; 
+            while(j - h >= 0 && aux < v[j-h]){
+                v[j] = v[j-h] ;
+                j -= h ; 
+            }
+            v[j] = aux ; 
+        }
+
     }
+
 
 }
 
@@ -28,12 +31,13 @@ int main(){
 
     int n ; scanf("%d", &n) ; 
 
-    int v[n+5] ;
-    for(int i = 0 ; i < n ; i++) scanf("%d", &v[i]);
+    int v[n+2] ;
+    
+    for(int i = 0 ; i < n ; i++) scanf("%d", &v[i]) ;
 
-    shell_sort(v, n) ;
+    shell(v, n) ;
 
     for(int i = 0 ; i < n ; i++) printf("%d ", v[i]) ;
-    printf("\n");   
-     
+    printf("\n") ;
+    
 }
