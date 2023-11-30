d0f0a50e55753a9e1ba120a1da8d581348ceb71c
Pietra <73311085+pijuma@users.noreply.github.com>
2023-11-30 15:03:01 -0300

    Create abb.h

---
+++ ED/árvore/arvore_bin_busca/abb.h
@@ -0,0 +1,19 @@
+#ifndef ABB_H
+    
+    #define ABB_H 
+    
+    #include "item.h"
+    #include<stdio.h> 
+    #include<stdlib.h>
+    #include<stdbool.h>
+
+    typedef struct ABB_ abb ; 
+
+    abb *ab_criar(); 
+    bool ab_inserir(abb*T, ITEM *item);
+    bool ab_remover(abb*T, int chave);
+    ITEM *abb_busca(abb *T, int chave);
+    void ab_apagar_arvore(abb **T);
+    void percurso(abb *T);
+
+#endif
