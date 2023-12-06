7cc09c864a9ceed2a41bb69e7993ca54a546dc4c
Pietra <73311085+pijuma@users.noreply.github.com>
2023-12-06 18:03:34 -0300

    Update heap.c

--- ED/fila_prioridade(heap)/heap.c
+++ ED/fila_prioridade(heap)/heap.c
@@ -1,9 +1,6 @@
-#include "heap.h"
-
-#define esq (2*at) + 1 
-#define dir (2*at) + 2
-
-// max heap 
+#include "pq.h"
+#define esq (2*i)+1 
+#define dir (2*i)+2
 
 struct heap_{
     ITEM *v[maxn] ;
@@ -11,75 +8,74 @@ struct heap_{
 } ;
 
 HEAP *heap_criar(){
-
-    HEAP *h = (HEAP *) malloc(sizeof(HEAP)) ;
-
-    if(h != NULL) h->lst = -1 ; 
-
-    return h ; 
-
-}
-
-int heap_cheia(HEAP *h){ return h->lst >= maxn ; }
-
-int heap_vazia(HEAP *h){ return h->lst == -1 ; }
-
-void swap(HEAP *h, int i, int j){
-    ITEM *t = h->v[j] ; 
-    h->v[j] = h->v[i] ; 
-    h->v[i] = t ; 
-}
-
-
-void fix_up(HEAP *h, int at){
-
-    while(at != 0){
-        int pai = (at-1)/2 ; 
-        if(item_get_chave(h->v[pai]) > item_get_chave(h->v[at])) break ; 
-        swap(h, pai, at) ; 
-        at = pai ; 
+    
+    HEAP *heap = (HEAP *) malloc(sizeof(HEAP)) ;
+    
+    if(heap != NULL){
+        heap->lst = -1 ;
     }
+    
+    return heap ; 
 
 }
 
-void fix_down(HEAP *h, int at){
+int heap_cheia(HEAP *h){ return h->lst == maxn - 1 ; }
 
-    while(at <= h->lst){
-        int mx = at ; 
-        if(item_get_chave(h->v[esq]) > item_get_chave(h->v[mx])) mx = esq ; 
-        if(item_get_chave(h->v[dir]) > item_get_chave(h->v[mx])) mx = dir ; 
-        if(mx == at) break ; 
-        swap(h, mx, at) ; 
-        at = mx ; 
+int heap_vazia(HEAP *h){ return h->lst == -1 ; } 
+
+void swap(HEAP *h, int a, int b){
+    ITEM *t = h->v[a] ; 
+    h->v[a] = h->v[b] ;
+    h->v[b] = t ; 
+}
+
+
+void fix_up(HEAP *h, int i){
+    while(i){
+        int pai = i-1 ; pai >>= 1 ; 
+        if(item_get_chave(h->v[i]) < item_get_chave(h->v[pai])) break ;
+        swap(h, i, pai) ;
+        i = pai ; 
     }
-
 }
 
 int heap_inserir(HEAP *h, ITEM *item){
-    // insere no lst + 1 - fix_up 
-    if(heap_cheia(h)) return 0 ; 
+    // insere no lst e faz fix_up
     int neww = h->lst + 1 ; 
     h->v[neww] = item ; 
+    h->lst++ ; 
     fix_up(h, neww) ;
-    h->lst = neww ; 
-    return 1 ; 
+}
+
+void fix_down(HEAP *h, int i){
+
+    int mx = i ; 
+
+    if(esq <= h->lst && item_get_chave(h->v[mx]) < item_get_chave(h->v[esq])) mx = esq ; 
+    if(dir <= h->lst && item_get_chave(h->v[mx]) < item_get_chave(h->v[dir])) mx = dir ;
+
+    if(mx == i) return ; 
+
+    swap(h, mx, i) ;
+    fix_down(h, mx) ;
+
 }
 
 int heap_remover(HEAP *h){
-    // remove topo -> joga last pro topo -> rearruma o resto descendo fix_down 
-    if(heap_vazia(h)) return 0 ; 
+    // troca toopo pelo lst e faz fix_down 
     swap(h, 0, h->lst) ;
     h->lst-- ; 
+    printf("%d\n", h->lst) ;
     fix_down(h, 0) ;
-    return 1 ; 
 }
 
 int topo(HEAP *h){ return item_get_chave(h->v[0]) ; }
 
 void show(HEAP *h){
-    for(int at = 0 ; at <= h->lst ; at++){
-        printf("sou %d:\n", item_get_chave(h->v[at])) ; 
-        if(esq <= h->lst)printf("tenho filho esq %d ", item_get_chave(h->v[esq])) ;
-        if(dir <= h->lst) printf("tenho filho direito %d\n",  item_get_chave(h->v[dir])) ; 
+    for(int i = 0 ; i <= h->lst/2 ; i++){
+        printf("Eu sou %d", item_get_chave(h->v[i])) ;
+        if(esq <= h->lst) printf(" meu esq eh %d", item_get_chave(h->v[esq])) ;
+        if(dir <= h->lst) printf(" meu direito eh %d", item_get_chave(h->v[dir])) ;
+        printf("\n") ; 
     }
 }
