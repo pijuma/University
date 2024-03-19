731896fc3f9ffa589d16822e888ba5d12244e6fe
Pietra <73311085+pijuma@users.noreply.github.com>
2024-03-18 21:57:04 -0300

    Update Main.java

--- POO/ex1/Main.java
+++ POO/ex1/Main.java
@@ -6,10 +6,21 @@ import java.util.Scanner;
 
 public class Main {
     static public void main(String[] agrs) {
+
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt() ;
-        Tabuleiro t1 = new Tabuleiro(n, n) ;
-        while(!t1.findSolution()){
+        int op = scanner.nextInt() ; // op = 1 entrada aleatoria/ op = 0 usuario digita
+
+        //considerar o 0 como posição vazia
+        // o usuario deve sempre colocar a posicao (0, 0) como vazia
+
+        Tabuleiro t1 = new Tabuleiro(n, n, op, scanner) ;
+        System.out.println("matriz inicial:\n");
+        t1.printMatrix();
+
+        // o usuario digita o movimento
+        while(!t1.findSolution()){ // enquanto nao achou solucao, continue movimentando
             char type = scanner.next().charAt(0);
             if(type == 'D') t1.getDown();
             else if(type == 'L') t1.getLeft();
