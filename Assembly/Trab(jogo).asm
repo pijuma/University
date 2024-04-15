2d13284112610c356f6f2325f58d77a2fa82945a
Pietra <73311085+pijuma@users.noreply.github.com>
2024-04-15 09:55:43 -0300

    Update Trab(jogo).asm

--- Assembly/Trab(jogo).asm
+++ Assembly/Trab(jogo).asm
@@ -1,6 +1,6 @@
-		.data
+.data
 		.align 0 
-		#frases de recepcão
+		# frases de recepcão
 inicial:	.asciz "Bem-vindo ao Jogo advinhe o numero! Seu objetivo e tentar advinhar o numero oculto fazendo chutes.\n"
 inicial2:	.asciz "A cada chute sera dado um veredito dizendo se o numero esta muito alto, muito baixo ou se voce esta correto.\n"
 inicial3:	.asciz "Bom jogo!\n" 
@@ -13,15 +13,14 @@ quebra_l: 	.asciz "\n"
 frase_fin:	.asciz "e os chutes foram: \n" 
 espaco:		.asciz " " 
 		.align 2 
-head: 		.space 4 #ponteiro para o inicio da lista -> no cabeca 
- 
+head: 		.space 4 # ponteiro para o inicio da lista -> no cabeca 
 		.text
 		.globl main 
  
 main: 
  
-	la t5, head #passar o end do head pro t5 temporariamente  
-	sw zero, 0(t5) #inicializando o head como null (lista vazia)
+	la t5, head # passar o end do head pro t5 temporariamente  
+	sw zero, 0(t5) # inicializando o head como null (lista vazia)
  
 	li a7, 4 
 	la a0, inicial # printar string
@@ -36,8 +35,10 @@ main:
 	addi t2, x0, 0 # t2 tem o valor de c 
 	addi t3, x0, 100 # t3 tem o valor de m 
  
-	add a1, x0, t3 # passa o valor de t3 para a1
- 
+	li a7, 30 #em a1 agora teremos uma seed aleatoria correspondente ao tempo
+	ecall 
+	mv a0, a1 #passando a seed pro a0 
+	addi a1, t3, 0
 	li a7, 42 # recebe em a0 um valor aleatório (será o valor inicial)
 	ecall 
  
@@ -47,23 +48,32 @@ main:
 # funcão para gerar um numero aleatorio utilizando o algoritmo de gerador contruencial linear 
 gen_linear: 
  
-	beq t4, zero, fim_gen_linear
+	beq t4, zero, fim_gen_linear # caso o contador t4 vire 0, encerra o gerador
 	j gerar_aleatorio # queremos gerar 2 numeros aleatorios (a e c)
 	fim_gerar:
 		mul t1, t1, t0 # a = a*xn 
 		add t1, t1, t2 # a = a + c 
-		rem t1, t1, t3 # a = a%m
+		rem t1, t1, t3 # a = a % m
 		add t0, t1, x0 # xn+1 = a 
 		addi t4, t4, -1 # decremento do loop 
 		j gen_linear
  
 gerar_aleatorio: 
  
+	li a7, 30 
+	ecall #gerar seed
+	mv a0, a1 
+	addi a1, t3, 0
 	li a7, 42 # ecall para gerar numero aleatorio - valor eh dado em 42
 	ecall 
-	add t1, a0, x0
+	li a7, 30 
+	ecall #gerar seed
+	mv a0, a1
+	add t1, a0, x0 # passar o numero aleatorio para t1 (a)
+	li a7, 42 
+	addi a1, t3, 0
 	ecall 
-	add t2, a0, x0
+	add t2, a0, x0 # passar outro numero aleatorio para t2 (c)
 	j fim_gerar 
  
  
@@ -79,7 +89,7 @@ loop_jogo:
 	add t6, x0, a0 # t6 guarda temporariamente o chute feito
 	jal add_node # jal para salvar o chute na lista encadeada
 	addi t1, t1, 1 # incrementa na quantidade de chutes feitos
-	blt t6, t0, less
+	blt t6, t0, less 
 	bgt t6, t0, greater
 	beq t6, t0, equal
  
@@ -118,7 +128,7 @@ loop_impressao:
 	li a7, 4
 	la a0, espaco # printar espaço
 	ecall 
-
+ 
 	lw t5, 4(t5) # mudar o valor de t5 para o novo endereco do proximo no
 	addi t1, t1, -1
  
@@ -147,7 +157,7 @@ greater:
 	li a7, 4
 	ecall
 	j loop_jogo
-
+ 
 # caso o chute seja igual o valor aleatório, pula para fim_jogo
 equal:
 	la a0, igual
@@ -163,9 +173,9 @@ add_node:
 	li a0, 8 # nosso no tem 8bytes (numero + end)
 	li a7, 9 # ecall para alocar uma nova memória
 	ecall # o endereco da nova memoria fica em a0
-	sw a0, 4(t5) # t5 (current) deve apontar para o no criado
-	sw t6, 0(a0) # o valor armazenado em a0 passa a ser o valor de t6(chute feito)
+	sw t6, 0(a0) # o valor armazenado no endereco a0 passa a ser o valor de t6(chute feito)
 	sw x0, 4(a0) # a0 deve apontar para NULL (nao existe proximo item)
+	sw a0, 4(t5) # armazena o endereco do novo no proximo de t5
 	mv t5, a0 # seta t5 como a0 -> passa a ser meu ultimo (current) 
 	jr ra 
  
@@ -174,10 +184,12 @@ primeiro_no:
 	li a0, 8 # nosso no tem 8bytes (numero + end)
 	li a7, 9 # ecall para alocar uma nova memória em a0
 	ecall # o endereco da nova memoria fica em a0
-	sw t6, 0(a0) # o valor armazenado em a0 passa a ser o valor de t6(chute feito)
-	sw x0, 4(a0) # a0 deve apontar para NULL (nao existe proximo item)
-	sw a0, 0(t5) # t5 (head nesse momento) tera o valor de a0 (ponteiro)
-	add t5, x0, a0 # o no t5 (current) passa a ser o no adicionado
+	sw t6, 0(a0) # o valor armazenado no endereco a0 passa a ser o valor de t6(chute feito)
+	sw x0, 4(a0) # endereco a0 deve apontar para NULL (nao existe proximo item)
+	sw a0, 0(t5) # passa o novo endereco (a0) para t5 (head nesse momento) 
+	mv t5, a0 # o no t5 (current) passa a ser o no adicionado
 	jr ra
+ 
+ 
  
  
